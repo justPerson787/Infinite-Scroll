33403702bfa6c79a7b91a9d1d67af81965869aba
N.U <n.n.ushakova@gmail.com>
2020-01-13 00:46:33 -0500

    Display data with JS

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
 // Unsplash API
 const count = 10;
 const apiKey = config.MY_KEY;
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
+//Create Elements For Links & Photos, Add to DOM
+function displayPhotos() {
+    //Run function for each object in photosArray
+    photosArray.forEach((photo) => {
+        //Create <a> to Link to Unsplash
+        const item = document.createElement('a');
+        item.setAttribute('href', photo.links.html);
+        item.setAttribute('target', '_blank'); /*openes link in a new window*/
+        //Create <img> for photo
+        const img = document.createElement('img');
+        img.setAttribute('src', photo.urls.regular);
+        img.setAttribute('title', photo.alt_description);
+        //Put <img> inside <a>, then put both inside imageContainer element
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+
+    });
+}
+
 // Get photos from Unsplash API
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
+        photosArray = await response.json();
+       displayPhotos();
     } catch(error) {
 
     }
