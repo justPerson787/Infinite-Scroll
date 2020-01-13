aa685fa67af1b610bf67f325120e7837cf2ca8d6
N.U <n.n.ushakova@gmail.com>
2020-01-13 00:28:47 -0500

    Finalize api data fetching

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Unsplash API
 const count = 10;
-const apiKey = '';
+const apiKey = config.MY_KEY;
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
 // Get photos from Unsplash API
